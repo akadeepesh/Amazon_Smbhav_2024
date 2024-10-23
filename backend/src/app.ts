becb53271f9d7cc9c0f48135dd082ba13778dc46
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2024-10-23 19:09:51 +0530

    Init backend & prettier config

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
