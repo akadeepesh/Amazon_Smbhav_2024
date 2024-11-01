42b90fceddf9e5778ff26114c61b859aa1a3bd83
nabanichowdhury <nabanichowdhury@gmail.com>
2024-11-02 00:24:18 +0530

    mongodb integrated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,10 @@ app.use('/api/users', userRoutes);
 // Global error handler
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send({
+    success: false,
+    message: err.message,
+  });
 });
 
 export default app;
